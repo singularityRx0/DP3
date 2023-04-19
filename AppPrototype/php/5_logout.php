cd5f1fda49081888520a915181d690d85b86f3ad
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2023-04-19 22:18:29 +0800

    Update 5_logout.php

--- AppPrototype/php/5_logout.php
+++ AppPrototype/php/5_logout.php
@@ -9,12 +9,12 @@
         $apikey = $_POST['apikey']; 
         $result = array();    
 
-        $sql = "select * from users where email = '".$email."' and apikey = '".$apikey."' ";
+        $sql = "select * from user_data where email = '".$email."' and apikey = '".$apikey."' ";
         $res = $conn->query($sql);
 
         if ($res->num_rows != 0) {
             $row = mysqli_fetch_assoc($res);
-            $sqlUpdate = "update users set apikey = '' where email = '".$email."' ";
+            $sqlUpdate = "update user_data set apikey = '' where email = '".$email."' ";
 
             if ($conn->query($sqlUpdate)) {
                 echo "success";
