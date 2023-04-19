a0f1dacc1d52914ffed371a7896e6f5378e9cb8d
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2023-04-19 22:22:59 +0800

    Create MainActivity.java

---
+++ AppPrototype/App_login module/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.appprototype1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textViewName = findViewById(R.id.name);
+        TextView textViewEmail = findViewById(R.id.email);
+
+        Button buttonGenerateQR = findViewById(R.id.generateQR);
+        Button buttonCall = findViewById(R.id.call);
+        Button buttonLogout = findViewById(R.id.logout);
+
+        sharedPreferences = getSharedPreferences("MyAppName", MODE_PRIVATE);
+        if (sharedPreferences.getString("logged", "false").equals("false")) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+
+        textViewName.setText(sharedPreferences.getString("name", ""));
+        textViewEmail.setText(sharedPreferences.getString("email", ""));
+
+
+
+        buttonGenerateQR.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), GenerateQRCode.class);
+            startActivity(intent);
+        });
+
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://192.168.0.108/AppPrototype/5_logout.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                if (response.equals("success")) {
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("logged", "");
+                                    editor.putString("name", "");
+                                    editor.putString("email", "");
+                                    editor.putString("apikey", "");
+                                    editor.apply();
+                                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else
+                                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("email", sharedPreferences.getString("email", ""));
+                        paramV.put("apikey", sharedPreferences.getString("apikey", ""));
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
+
+        buttonCall.setOnClickListener(view -> {
+            Intent intent ;
+        });
+
+
+    }
+}
