38492436c62fe7b69e0f6e7d0a30b39a2efc138e
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2023-04-19 18:51:30 +0800

    Update 1_connectDB.php

--- AppPrototype/php/1_connectDB.php
+++ AppPrototype/php/1_connectDB.php
@@ -2,7 +2,7 @@
 
     $servername = "localhost";
     $username = "root";		//put your phpmyadmin username.(default is "root")
-    $password = "012862871105232000lzH@";			//if your phpmyadmin has a password put it here.(default is "root")
+    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
     $dbname = "appprototype";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
