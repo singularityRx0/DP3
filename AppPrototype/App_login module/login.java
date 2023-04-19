7cdeeb86d4d23d1135e1da5db88a7e68e1a7880a
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2023-04-19 22:30:39 +0800

    Create login.java

---
+++ AppPrototype/App_login module/login.java
@@ -0,0 +1,126 @@
+package com.example.appprototype1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends AppCompatActivity {
+
+    private TextInputEditText textInputEditTextEmail, textInputEditTextPassword;
+    private String email, password, name, apikey;
+
+    ProgressBar progressBar;
+
+    SharedPreferences sharedPreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView textViewRegister = findViewById(R.id.register_now);
+        TextView textViewError = findViewById(R.id.error);
+
+        progressBar = findViewById(R.id.loading);
+
+        textInputEditTextEmail = findViewById(R.id.email);
+        textInputEditTextPassword = findViewById(R.id.password);
+        Button buttonSubmit = findViewById(R.id.submit);
+
+        sharedPreferences = getSharedPreferences("MyAppName", MODE_PRIVATE);
+
+        if (sharedPreferences.getString("logged", "false").equals("true")) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = String.valueOf(textInputEditTextEmail.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://192.168.0.108/AppPrototype/3_login.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    String status = jsonObject.getString("status");
+                                    String message = jsonObject.getString("message");
+                                    if (status.equals("success")){
+                                        name = jsonObject.getString("name");
+                                        email = jsonObject.getString("email");
+                                        apikey = jsonObject.getString("apikey");
+
+                                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                                        editor.putString("logged", "true");
+                                        editor.putString("name", name);
+                                        editor.putString("email", email);
+                                        editor.putString("apikey", apikey);
+                                        editor.apply();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else {
+                                        textViewError.setText(message);
+                                        textViewError.setVisibility(View.VISIBLE);
+                                   }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressBar.setVisibility(View.GONE);
+                        textViewError.setText(error.getLocalizedMessage());
+                        textViewError.setVisibility(View.VISIBLE);
+                    }
+                }){
+                    protected Map<String, String> getParams(){
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("email", email);
+                        paramV.put("password", password);
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registration.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
