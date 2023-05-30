623ab6d30f367cd660588e11e522ac587d98b8f2
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2023-05-30 23:52:11 +0800

    Update profile.php

--- android_api_test/profile.php
+++ android_api_test/profile.php
@@ -1,6 +1,6 @@
 <?php
 if (!empty($_POST['email']) && !empty($_POST['apikey'])) {
-    $con = mysqli_connect(hostname:"localhost" , username:"root" , password:"012862871105232000lzH@" ,database:"login_register");
+    $con = mysqli_connect(hostname:"localhost" , username:"root" , password:"@" ,database:"login_register");
 
     $email = $_POST['email'];
     $apikey = $_POST['apikey']; 
